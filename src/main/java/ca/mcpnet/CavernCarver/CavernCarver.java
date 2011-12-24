5ba7795d6145e38bea71742723122c3b2746d270
Michael Gentili <gentili@mcpnet.ca>
2011-12-23 22:28:47 -0500

    Fix version numbers

--- src/main/java/ca/mcpnet/CavernCarver/CavernCarver.java
+++ src/main/java/ca/mcpnet/CavernCarver/CavernCarver.java
@@ -9,20 +9,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class CavernCarver extends JavaPlugin {
 
+	static public final String VERSION = "0.2";
 	static Logger logger = Logger.getLogger("Minecraft");
 	static public void log(String msg) {
 		logger.info("[CavernCarver] "+msg);
 	}
 	
 	public void onEnable() {
-		log("CavernCarver 0.0.1 Plugin Enabled!");
+		log("CavernCarver v"+VERSION+" Plugin Enabled!");
 		getConfig().options().copyDefaults(true);
 		getConfig().options().copyHeader(true);
 		saveConfig();
 	}
 
 	public void onDisable() {
-		log("CavernCarver 0.0.1 Plugin Disabled!");		
+		log("CavernCarver v"+VERSION+" Plugin Disabled!");		
 	}
 
 	@Override
